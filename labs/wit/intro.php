afff210f2510876d44189bfaa77a5511c17babc7
sambhav saxena <sambhavsaxena02@gmail.com>
2022-06-08 12:47:46 +0530

    jhbidka

--- labs/wit/intro.php
+++ labs/wit/intro.php
@@ -3,4 +3,9 @@ echo "<h2>PHP is Fun!</h2>";
 echo "Hello world!<br>";
 echo "I'm about to learn PHP!<br>";
 echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+
+$age = array("Name1"=> "20", "Name2"=> "21", "Name3"=> "30");
+
+echo "$age[1]";
+
 ?>
